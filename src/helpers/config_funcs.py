f239d9948479b8b335e504e7e2cb915c144500b2
brandontkessler <brandontkessler@gmail.com>
2021-12-31 14:15:43 -0800

    altered default path

--- src/helpers/config_funcs.py
+++ src/helpers/config_funcs.py
@@ -12,27 +12,28 @@ def reset_kenfig():
     shutil.copyfile('templates/kenfile.yaml', 'Kenfig')
 
 
-def set_state(path=os.path.expanduser('~')):
-    if not os.path.exists(os.path.join(path, '.ken')):
-        os.mkdir(os.path.join(path, '.ken'))
+def set_state(path=os.path.join(os.path.expanduser('~'), '.ken')):
+    if not os.path.exists(path):
+        os.mkdir(path)
     
-    with open(os.path.join(path, '.ken', 'state'), 'w+') as f:
+    with open(os.path.join(path, 'state'), 'w+') as f:
         yaml.dump(get_kenfig(), f)
 
 
-def get_state(path=os.path.expanduser('~')):
-    with open(os.path.join(path, '.ken', 'state'), 'r') as f:
+def get_state(path=os.path.join(os.path.expanduser('~'), '.ken', 'state')):
+    with open(path, 'r') as f:
         return yaml.safe_load(f)
 
-def edit_state(section, key, value, path=os.path.expanduser('~')):
+def edit_state(section, key, value, 
+               path=os.path.join(os.path.expanduser('~'), '.ken', 'state')):
     state = get_state()
     state[section][key] = value
 
-    with open(os.path.join(path, '.ken', 'state'), 'w') as f:
+    with open(path, 'w') as f:
         yaml.dump(state, f, default_flow_style=False)
 
 
-def coalesce_state_with_kenfig(path=os.path.expanduser('~')):
+def coalesce_state_with_kenfig(path=os.path.join(os.path.expanduser('~'), '.ken', 'state')):
     new_opts = get_kenfig()
     state = get_state()
 
@@ -43,11 +44,11 @@ def coalesce_state_with_kenfig(path=os.path.expanduser('~')):
             if v is not None:
                 state[section][k] = v
     
-    with open(os.path.join(path, '.ken', 'state'), 'w') as f:
+    with open(path, 'w') as f:
         yaml.dump(state, f, default_flow_style=False)
 
 def get_subscription_options():
-    with open(os.path.join('templates/kenfile.yaml'), 'r') as template:
+    with open(os.path.join('templates', 'kenfile.yaml'), 'r') as template:
         return yaml.safe_load(template)['subscriptions'].keys()
 
 if __name__=='__main__':
